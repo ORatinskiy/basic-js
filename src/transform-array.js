7b7104b7923af575782e0350941ce4013d3a99ef
ORatinskiy <234854@gmail.com>
2020-10-21 08:44:30 +0300

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] === 'string') {
+            if (i !== (array.length - 1)) {
+                if (array[i] === '--discard-next') {
+                    i++;
+                } else if (array[i] === '--discard-prev') {
+                    result = result.slice(0, -1);
+                } else if (array[i] === '--double-next') {
+                    result.push(array[i + 1]);
+                } else if (array[i] === '--double-prev') {
+                    //result = result[ - 1];
+                }
+            } else {
+                result = result.slice(0, -1);
+            }
+
+        } else {
+            result.push(array[i]);
+        }
+    }
+    return result;
 };
